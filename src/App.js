ec063dd35e64638559c848b29ed0261d2e92cf61
payls <payalsonawane@gmail.com>
2021-03-11 12:16:58 +0530

    bank on us assignment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Services from './components/productService';
+
+function App() {
+  return (
+    <div className="bou-main">
+      <Services />
+    </div>
+  );
+}
+
+export default App;
